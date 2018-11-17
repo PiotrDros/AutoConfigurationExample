9c5ebdd3bf07053eb4f0e661ed680c8e3a722071
piotrdros@gmail.com <piotrdros@gmail.com>
2018-11-17 18:59:36 +0100

    Initial commit

---
+++ Library/src/main/java/com/library/HomeConfiguration.java
@@ -0,0 +1,9 @@
+package com.library;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan
+public class HomeConfiguration {
+}
